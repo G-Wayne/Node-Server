7ee5a0782d6b5664be0e1e895ee29f5c8726340c
Gawayne Lawton <gawayne_lawton@yahoo.com>
2018-10-25 13:03:19 -0500

    Basic routing

--- app.js
+++ app.js
@@ -4,14 +4,21 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res){
   console.log('request was made from: ' + req.url);
-  res.writeHead(200, {'Content-Type': 'application/json'});
-  var myObj = {
-    name:'William',
-    job: 'Biologist',
-    age: 24
-  };
-  //convert to json format and send to client
-  res.end(JSON.stringify(myObj));
+  if (req.url === '/home' || req.url === '/'){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.createReadStream(__dirname + '/index.html').pipe(res);
+  } else if (req.url === '/contact'){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.createReadStream(__dirname + '/contact.html').pipe(res);
+  } else if (req.url === '/api/employees') {
+    var employees = [{name: 'Thomas', age: 25}, {name: 'Crystal', age: 33}];
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    //convert to json format and send to client
+    res.end(JSON.stringify(employees));
+  } else {
+    res.writeHead(404, {'Content-Type': 'text/html'});
+    fs.createReadStream(__dirname + '/404.html').pipe(res);
+  }
 });
 
 //sepecify port for request

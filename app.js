2ca3c23ed4d590a0ef9dc8f4991d518365d4e9d7
Gawayne Lawton <gawayne_lawton@yahoo.com>
2018-10-25 18:36:20 -0500

    Adding express

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
 
+//setup express app
+var app = express();
 
-var server = http.createServer(function(req, res){
-  console.log('request was made from: ' + req.url);
-  if (req.url === '/home' || req.url === '/'){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.createReadStream(__dirname + '/index.html').pipe(res);
-  } else if (req.url === '/contact'){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.createReadStream(__dirname + '/contact.html').pipe(res);
-  } else if (req.url === '/api/employees') {
-    var employees = [{name: 'Thomas', age: 25}, {name: 'Crystal', age: 33}];
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    //convert to json format and send to client
-    res.end(JSON.stringify(employees));
-  } else {
-    res.writeHead(404, {'Content-Type': 'text/html'});
-    fs.createReadStream(__dirname + '/404.html').pipe(res);
-  }
+//respond to get request
+app.get('/', function(req, res){
+  res.send('This is the homepage');
 });
 
-//sepecify port for request
-server.listen(3000, '127.0.0.1');
-console.log('Server now listening on port 3000');
+app.get('/contact', function(req, res){
+  res.send('This is the contact page');
+});
+
+
+//listen to port
+app.listen(3000);

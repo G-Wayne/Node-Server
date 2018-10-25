8795567e1102e9ad5f9a22bebb0a88f7a2b5bbe4
Gawayne Lawton <gawayne_lawton@yahoo.com>
2018-10-25 18:52:31 -0500

    Adding route params

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ app.get('/contact', function(req, res){
   res.send('This is the contact page');
 });
 
+//adding a dynamic route
+app.get('/profile/:name', function(req, res){
+  res.send('You requested to see a profile with the name of ' + req.params.name);
+});
+
 
 //listen to port
 app.listen(3000);

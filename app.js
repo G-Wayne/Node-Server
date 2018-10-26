a38563466740cffe8f1293a23c2dd332df415cb1
Gawayne Lawton <gawayne_lawton@yahoo.com>
2018-10-25 19:48:29 -0500

    Adding and using template engine

--- app.js
+++ app.js
@@ -3,18 +3,23 @@ var express = require('express');
 //setup express app
 var app = express();
 
+//set ejs as view engine
+app.set('view engine', 'ejs');
+
 //respond to get request
 app.get('/', function(req, res){
-  res.send('This is the homepage');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/contact', function(req, res){
-  res.send('This is the contact page');
+  res.sendFile(__dirname + '/contact.html');
 });
 
 //adding a dynamic route
 app.get('/profile/:name', function(req, res){
-  res.send('You requested to see a profile with the name of ' + req.params.name);
+  //render view
+  var data = {age:26, job: 'Botanist'};
+  res.render('profile',{person: req.params.name, data: data});//set property person to output object of profile
 });
 
 
